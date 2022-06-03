3999ac480d8b4a5e4dcb52c8727183f7f4b34c49
Skyview <>
2022-06-03 16:41:00 +0000

    Added Files

--- main.sh
+++ main.sh
@@ -1,13 +1,12 @@
-echo Starting up!
-echo Compiling EPK...
-java -jar epkcompiler/CompilePackage.jar "resources" "output/assets.epk";
-echo Compiled EPK
-echo Removing Classes.js
-rm output/classes.js
-rm output/classes.js.map
-echo Compiling classes.js...
-sh gradlew teavmc
-echo Compiled classes.js
-echo Starting Server for Testing...
-python -m http.server --directory output
-echo Finished.
+echo Welcome to DevForMOV! What would you like to do?
+echo 1: Compile Texture Pack
+echo 2: Compile Client
+echo 3: Start Server for Testing
+echo 4: Push All Changes to Github
+read -p "Answer:" ans
+case $ans in
+  1 ) echo Compiling EPK...; java -jar epkcompiler/CompilePackage.jar "resources" "output/assets.epk"; echo Compiled EPK; exit;;
+  2 ) echo Compiling Client...; sh gradlew teavmc; echo Compiled classes.js; exit;;
+  3 ) echo Starting Server for Testing...; python -m http.server --directory output; exit;;
+  4 ) git init; git add -A; git commit -m 'Added Files'; git remote add origin git@github.com:TheRealUnBot/MinecraftOfVPS.git; git push -u -f origin main; exit;;
+esac
